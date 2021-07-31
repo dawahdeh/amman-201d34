c8164114a94f27e43963d0d4d5919688eb7ae9d0
Mohammad Dawahdeh <m.i.dawahdeh@gmail.com>
2021-07-30 18:31:34 -0900

    problem 3 solution

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -12,13 +12,10 @@ and then returns an array where the first element is the sum of
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { 
-  //eslint-disable-line
+function sum(a, b) { //eslint-disable-line
   let total = a + b;
   return [total,  `The sum of ${a} and ${b} is ${total}.`];
 }
-
-let xxx=50;
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
@@ -27,44 +24,72 @@ testSum(4, 7);
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the
- first element is the product of those numbers, and the second element is a string that EXACTLY follows 
- this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as arguments and returns an array where the 
+first element is the product of those numbers, and the second element is a string that EXACTLY follows 
+this example and uses the values that were input into the function:
 
 "The product of 5 and 9 is 45."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the 
-call for the testMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-let product=a*b;
-  return [product, `The product of ${a} and ${b} is ${product}.`];
+
+  let product =a*b;
+
+  return [product,`The product of ${a} and ${b} is ${product}.`]
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
- testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and 
+returns an array where the first element is the sum of those three numbers, 
+the second element is the product of those three numbers,  
+and the third and fourth elements are strings that EXACTLY follow this 
+example and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() 
+function, and to do multiplication, use your multiply() function that you've already created.
+ You're going to have to be resourceful to figure out how to do this. However, 
+ you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment 
+the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+
+
+   
+   let sum1 =sum(a, b);
+   let sum2=sum(sum1[0],c);
+
+console.log(sum2[0]);
+let Total=sum2[0];
+let product1=1;
+
+ product1=multiply(a, b);
+let product =multiply(product1[0], c);
+let Product=product[0];
+
+  return [Total,Product,`${a} and ${b} and ${c} sum to ${Total}.`,`The product of ${a} and ${b} and ${c} is ${Product}.`];
+
+
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -104,10 +129,12 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
+  
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ //testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
